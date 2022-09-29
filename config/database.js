9fde8bae9b0dbc994fbca52d6e57c6225d957963
Nico Calvo <calvonico@users.noreply.github.com>
2022-09-29 20:14:42 +0100

    Configured production/database.js

--- config/database.js
+++ config/database.js
@@ -16,11 +16,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres', 
   connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
+        host: env('DATABASE_HOST', '159.65.16.204'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
+        database: env('DATABASE_NAME', 'basedatos'),
+        user: env('DATABASE_USERNAME', 'sammy'),
+        password: env('DATABASE_PASSWORD', 'password'),
         ssl: {
           rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
        },
