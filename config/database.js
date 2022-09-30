d4a6f57d0f79536b7a49ec81140736b65c5615f4
Nico Calvo <calvonico@users.noreply.github.com>
2022-09-30 18:45:20 +0100

    Configured production/database.json

--- config/database.js
+++ config/database.js
@@ -18,8 +18,8 @@ module.exports = ({ env }) => ({
   connection: {
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'basedatos'),
-        user: env('DATABASE_USERNAME', 'sammy'),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'password'),
         ssl: {
           rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
