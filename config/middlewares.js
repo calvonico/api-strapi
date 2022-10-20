8c70d2467a758c506e420b07193ac4d3f14f6cc6
Nico Calvo <calvonico@users.noreply.github.com>
2022-10-20 21:18:34 +0100

    agregados plugins

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,9 +6,12 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          'connect-src': ["'self'", 'https:', "*.tinymce.com", "*.tiny.cloud", "blob:"],
+          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com', "*.tinymce.com", "*.tiny.cloud", "cdn.jsdelivr.net", "strapi.io", "s3.amazonaws.com"],
           'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          "script-src": ["'self'", "*.tinymce.com", "*.tiny.cloud", "https:"],
+          "style-src": ["'self'", "'unsafe-inline'", "*.tinymce.com", "*.tiny.cloud"],
+          "font-src": ["'self'", "*.tinymce.com", "*.tiny.cloud"],
           upgradeInsecureRequests: null,
         },
       },
